465e90983602a8aa52cc00c77220947bd7d26216
VitorFichel <vitorfichel@gmail.com>
2025-08-21 00:05:58 -0300

    Project hr-api-gateway-zuul created

---
+++ hr-api-gateway-zuul/src/test/java/com/fichel/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fichel.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
